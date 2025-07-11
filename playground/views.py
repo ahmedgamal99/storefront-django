d458d8d7d25f6db28ebc9e82c070f6b8e79e67eb
Ahmed Gamal <victorylover987@gmail.com>
2025-07-11 17:18:45 -0400

    Created admin panel and registerd product and collection models

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-# request -> response // aka request handler
 
 def say_hello(request):
-    x,y = 1,2
-    return render(request, 'hello.html', {'name' : "Jimmy"})
+    return render(request, 'hello.html', {'name': 'Mosh'})
