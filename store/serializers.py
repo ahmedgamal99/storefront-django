114908bba86d73ee1b5bd6a852d0455420d5eb34
Ahmed Gamal <victorylover987@gmail.com>
2025-07-14 16:28:05 -0400

    Added views and serializers for product and collection models

---
+++ store/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from decimal import Decimal
+from .models import Product, Collection
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length = 255)
+
+class ProductSerializer(serializers.ModelSerializer):
+    '''
+    { "title" : "a", "slug" : "a" , "collection" : 1, "unit_price" : 1, "inventory" : 1 }
+    '''
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length = 255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name = 'collection-detail'
+    # )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+    
