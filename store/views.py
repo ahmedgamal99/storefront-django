13c0ce536a29d4d841aa1f723cfb31c073e11267
Ahmed Gamal <victorylover987@gmail.com>
2025-07-15 15:09:29 -0400

    Added pagination

--- store/views.py
+++ store/views.py
@@ -8,6 +8,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 # Create your views here.
 
 class ProductViewSet(ModelViewSet):
@@ -15,17 +16,9 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ["unit_price", "last_update"]
-
-    # def get_queryset(self):
-    #     queryset = Product.objects.all()
-    #     collection_id = self.request.query_params.get('collection_id')
-    #     if collection_id is not None:
-    #         queryset = Product.objects.filter(collection_id = collection_id)
-        
-    #     return queryset
-    
     def get_serializer_context(self):
         return {"request" : self.request}
     
