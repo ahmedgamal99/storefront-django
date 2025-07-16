b1ad5e2fddf4483e725385443a4c7681dc447c26
Ahmed Gamal <victorylover987@gmail.com>
2025-07-15 21:24:15 -0400

    Created cart model, view, and serializer: cart and cartItems

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from decimal import Decimal
-from .models import Product, Collection, Review
+from .models import Product, Collection, Review, Cart, CartItem
+from pprint import pprint
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,15 +19,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length = 255)
-    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset = Collection.objects.all(),
-    #     view_name = 'collection-detail'
-    # )
-
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')    
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
@@ -38,3 +31,39 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
+
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price'] 
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField(method_name='calculate_total_price')
+
+    def calculate_total_price(self, cart_item : CartItem):
+        return cart_item.product.unit_price * cart_item.quantity
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price']
+
+    id = serializers.UUIDField(read_only = True)
+    items = CartItemSerializer(many = True)
+
+    total_price = serializers.SerializerMethodField(method_name='calculate_total_price')
+
+    def calculate_total_price(self, cart : Cart):
+        total = 0
+        for item in cart.items.all():
+            total += item.quantity * item.product.unit_price
+        return total
+
+
+
--- store/views.py
+++ store/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
-from .models import Product, Collection, OrderItem, Review
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import Product, Collection, OrderItem, Review, Cart
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
 from .filters import ProductFilter
 from .pagination import DefaultPagination
 # Create your views here.
@@ -47,7 +48,9 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
 
-
+class CartViewSet(ModelViewSet, CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
 
 
 
